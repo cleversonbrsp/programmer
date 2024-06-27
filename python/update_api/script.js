99057995e250a1e6c3820422d6bfbac10560ad39
Cleverson Rodrigues <thefly72003@yahoo.com.br>
2024-06-27 13:16:22 -0300

    atz

--- python/update_api/script.js
+++
@@ -1,5 +0,0 @@
-const iniciarAtualizacaoButton = document.getElementById('iniciar-atualizacao');
-
-iniciarAtualizacaoButton.addEventListener('click', () => {
-    window.location.href = '/executar-atualizacao';
-});
--- python/update_api/scripts/update_binaries.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Copiar e sobrepor os arquivos binários
-cp -r /path/to/extracted/binaries/* /path/to/website/files/
