85ded3308bfde2dd081e3460f6b4d3b38c91aa51
Cleverson Rodrigues <cleverson.ccna@gmail.com>
2025-03-09 20:43:41 -0300

    desafio 36

--- python/guanabara/mundo-2/036.py
+++ python/guanabara/mundo-2/036.py
@@ -4,3 +4,21 @@
 # O programa vai perguntar o valor da casa, o salario do comprador e em quantos anos ele vai pagar.
 # Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
 
+casa = float(input('Qual o valor da casa? R$ '))
+salario = float (input('Qual o seu salário? R$ '))
+anos = input('Em quantos anos você pretende pagar? ')
+
+if float(salario) * 0.3 > float(casa) / (int(anos) * 12):
+    print('Emprestimo aprovado!')
+    print('O valor da casa é de R$ {:.2f}'.format(float(casa)))
+    print('O valor do salário é de R$ {:.2f}'.format(float(salario)))
+    print('O valor da prestação mensal é de R$ {:.2f}'.format(float(casa) / (int(anos) * 12)))
+    print('O valor da prestação mensal não excede 30% do seu salário.')
+else:
+    print('Emprestimo negado!')
+    print('O valor da casa é de R$ {:.2f}'.format(float(casa)))
+    print('O valor do salário é de R$ {:.2f}'.format(float(salario)))
+    print('O valor da prestação mensal é de R$ {:.2f}'.format(float(casa) / (int(anos) * 12)))
+    print('O valor da prestação mensal não excede 30% do seu salário.')
+
+
