3ec5a6a99a92f2ed3b961ce2de7d6f9db6c97b2c
Cleverson Rodrigues <cleverson.rodrigues@enghouse.com>
2025-04-04 22:34:29 -0300

    java day

---
+++ cursoemvideo/java/Exercícios de Java #03.md
@@ -0,0 +1,2 @@
+https://www.cursoemvideo.com/curso/java-basico/aulas/java/modulos/exercicios-de-java-03/
+
---
+++ cursoemvideo/java/java-handson/src/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Meu primeiro programa em Java!");
+    }
+}
+
+// de baixo para cima: eu tenho um metodo principal,
+// que esta dentro da classe PrimeiroPrograma
+// que faz parte do pacote primeiroprograma.
+
+//Ou seja, eu tenho um metodo dentro de uma classe, que faz parte de um pacote.
+// O nome do arquivo deve ser o mesmo nome da classe, ou seja, PrimeiroPrograma.java
